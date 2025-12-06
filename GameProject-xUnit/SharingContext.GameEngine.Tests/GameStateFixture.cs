02bf2a442776cd17bbe2afb407e66bcbdafa3991
Anish1987 <anishnjain@gmail.com>
2025-12-06 12:44:07 +0530

    Added project related to xunit

---
+++ GameProject-xUnit/SharingContext.GameEngine.Tests/GameStateFixture.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameEngine.Tests
+{
+    public class GameStateFixture : IDisposable
+    {
+        //moCk
+
+        public GameState State { get; private set; }
+
+        public GameStateFixture()
+        {
+            State = new GameState();
+        }
+
+        public void Dispose()
+        {
+            // Cleanup
+        }
+    }
+}
